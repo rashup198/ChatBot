0a5181b2b739cc1b99cd5c82dc22549664ac230f
Priyanshu Kumar Pandey <priyanshukum198@gmail.com>
2025-01-19 11:32:56 +0530

    update the chatMessage

--- chat-bot/src/components/ChatMessage.jsx
+++ chat-bot/src/components/ChatMessage.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import ChatbotIcon from './ChatbotIcon'
-
-const ChatMessage = ({chat}) => {
+import ChatbotIcon from "./ChatbotIcon";
+const ChatMessage = ({ chat }) => {
   return (
-      <div className={`message ${chat.role === "model" ? 'bot' : 'user'}-message`}>
-            {chat.role === "model" && <ChatbotIcon></ChatbotIcon>}
-            <p className='message-text'> {chat.text}</p>
+    !chat.hideInChat && (
+      <div className={`message ${chat.role === "model" ? "bot" : "user"}-message ${chat.isError ? "error" : ""}`}>
+        {chat.role === "model" && <ChatbotIcon />}
+        <p className="message-text">{chat.text}</p>
       </div>
-  )
-}
-
-export default ChatMessage
+    )
+  );
+};
+export default ChatMessage;
